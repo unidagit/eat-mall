a11322c599b99e0dd39bb9637a1ef047c039e97a
unidagit <rhcjrdbsgml@gmail.com>
2023-02-07 00:33:38 +0900

    Chore : image 주소 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["s3.ap-northeast-2.amazonaws.com"],
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
