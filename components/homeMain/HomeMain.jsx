697570931d50cf0c4eeeebe0305c808e576e9bce
unidagit <rhcjrdbsgml@gmail.com>
2023-02-07 00:37:12 +0900

    Feat : HomeMain UI 기능 추가

---
+++ components/homeMain/HomeMain.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomeMain() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover customImg"></div>
+  );
+}
+
+export default HomeMain;
