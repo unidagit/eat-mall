0aca2ab933c85b7b6f913c47cf1b40ad2e2a802e
unidagit <rhcjrdbsgml@gmail.com>
2023-02-07 00:38:20 +0900

    Feat : footer UI 기능 추가

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className=" w-full flex justify-center items-center h-20 bg-black ">
+      <p className="text-white">@2023 조윤희</p>
+    </div>
+  );
+}
+
+export default Footer;
